bae07dae829891ccf41930ffaa5a794689e6406e
Sven Hodapp <sven.hodapp@gmail.com>
2016-06-06 16:17:01 +0200

    AccumuloFeederSpec includes now a more meaningful test case for the I2B Relation Marginal Note Example.

--- src/test/scala/AccumuloFeederSpec.scala
+++ src/test/scala/AccumuloFeederSpec.scala
@@ -28,9 +28,43 @@ class AccumuloFeederSpec(_system: ActorSystem)
     system.awaitTermination(10.seconds)
   }
 
-  "An AccumuloFeeder" should "be able write a (BEL) Cropus into Accumulo" in {
-    val af = TestActorRef(Props[AccumuloFeeder])
+  "I2B Relation Marginal Note Example" should "have a scaiview.abstract and some meta data" in {
 
+    val af = TestActorRef(Props[AccumuloFeeder])
+    var artifacts = Seq[KnowledgeArtifact]()
+
+    // Note: scaiview.abstract means title tab tab text
+    artifacts = KnowledgeArtifact(
+      new URI("header/pmid:25690317"),
+      new URI("_"),
+      new URI("header/header"),
+      "Investigating the genetic basis of fever-associated syndromic epilepsies using copy number variation analysis.\t\tFever-associated syndromic epilepsies ranging from febrile seizures plus (FS+) to Dravet syndrome have a significant genetic component. However, apart from SCN1A mutations in >80% of patients with Dravet syndrome, the genetic underpinnings of these epilepsies remain largely unknown. Therefore, we performed a genome-wide screening for copy number variations (CNVs) in 36 patients with SCN1A-negative fever-associated syndromic epilepsies. Phenotypes included Dravet syndrome (n = 23; 64%), genetic epilepsy with febrile seizures plus (GEFS+) and febrile seizures plus (FS+) (n = 11; 31%) and unclassified fever-associated epilepsies (n = 2; 6%). Array comparative genomic hybridization (CGH) was performed using Agilent 4 × 180K arrays. We identified 13 rare CNVs in 8 (22%) of 36 individuals. These included known pathogenic CNVs in 4 (11%) of 36 patients: a 1q21.1 duplication in a proband with Dravet syndrome, a 14q23.3 deletion in a proband with FS+, and two deletions at 16p11.2 and 1q44 in two individuals with fever-associated epilepsy with concomitant autism and/or intellectual disability. In addition, a 3q13.11 duplication in a patient with FS+ and two de novo duplications at 7p14.2 and 18q12.2 in a patient with atypical Dravet syndrome were classified as likely pathogenic. Six CNVs were of unknown significance. The identified genomic aberrations overlap with known neurodevelopmental disorders, suggesting that fever-associated epilepsy syndromes may be a recurrent clinical presentation of known microdeletion syndromes.".stripMargin.getBytes,
+      Meta(new URI("scaiview.abstract"), 1)
+    ) +: artifacts
+
+    artifacts = KnowledgeArtifact(
+      new URI("header/pmid:25690317"),
+      new URI("_"),
+      new URI("header/authors"),
+      """Hartmann et al.""".stripMargin.getBytes,
+      Meta(new URI("freetext"), 1)
+    ) +: artifacts
+
+    artifacts = KnowledgeArtifact(
+      new URI("header/pmid:25690317"),
+      new URI("_"),
+      new URI("header/publicationDate"),
+      """2015-03""".stripMargin.getBytes,
+      Meta(new URI("freetext"), 1)
+    ) +: artifacts
+
+    af ! DocElem2Accumulo(Corpus(artifacts))
+
+  }
+
+  it should "have some tiny BEL relation documents as marginal notes" in {
+
+    val af = TestActorRef(Props[AccumuloFeeder])
     var artifacts = Seq[KnowledgeArtifact]()
 
     artifacts = KnowledgeArtifact(
@@ -63,7 +97,14 @@ class AccumuloFeederSpec(_system: ActorSystem)
       Meta(new URI("bel@v1.0"), 3)
     ) +: artifacts
 
-    // annotations for highlighting annotations "position vectors"
+    af ! DocElem2Accumulo(Corpus(artifacts))
+
+  }
+
+  it should "have annotations with position vectors for the highlighting" in {
+
+    val af = TestActorRef(Props[AccumuloFeeder])
+    var artifacts = Seq[KnowledgeArtifact]()
 
     artifacts = KnowledgeArtifact(
       new URI("header/pmid:25690317"),
@@ -100,7 +141,16 @@ class AccumuloFeederSpec(_system: ActorSystem)
       Meta(new URI("annotation@v1"), 300)
     ) +: artifacts
 
-    // TODO: Only for the search
+    af ! DocElem2Accumulo(Corpus(artifacts))
+
+  }
+
+  it should "have annotations for helping the big-table search/scan/seek" in {
+
+    pending
+
+    val af = TestActorRef(Props[AccumuloFeeder])
+    var artifacts = Seq[KnowledgeArtifact]()
 
     // artifacts = KnowledgeArtifact(
     //   new URI("header/pmid:25690317"),
@@ -135,6 +185,7 @@ class AccumuloFeederSpec(_system: ActorSystem)
     // ) +: artifacts
 
     af ! DocElem2Accumulo(Corpus(artifacts))
+
   }
 
 }
